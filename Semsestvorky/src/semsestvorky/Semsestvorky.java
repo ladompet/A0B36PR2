24eb31034478a445d15d4b4c89dc09b00b7d1692
Piotr04_SVK <Piotr04_SVK@147.32.218.245>
2013-02-28 13:30:30 +0100

    skuska

--- Semsestvorky/src/semsestvorky/Semsestvorky.java
+++ Semsestvorky/src/semsestvorky/Semsestvorky.java
@@ -14,6 +14,6 @@ public class Semsestvorky {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("bla");
     }
 }
